0c601c2927bfc7a9a663638c90a65c552780d7b2
taciano <tacianohora2020@hotmail.com>
2023-05-17 22:34:17 -0300

    feat: configurar typescript e styled-components

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Login() {
+  return (
+    <Container>
+      <Title>Hello World</Title>
+    </Container>
+  );
+}
